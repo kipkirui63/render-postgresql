ccc57d5bb202e8096ce312b81bc0f5fecefd317a
kipkirui63 <enocksang8356@gmail.com>
2023-10-17 18:21:04 +0300

    updated

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://win_e1_app_user:u1IAmbprNtRfLsPT4dRvBaW459s7pOFD@dpg-ckn7lbh1rp3c739i37j0-a.ohio-postgres.render.com/win_e1_app'
 db = SQLAlchemy(app)
